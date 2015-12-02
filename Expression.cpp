e97f89e22ffe459a83ace48060edc29f4380576f
VictorCarri <victorcarri@gmail.com>
2015-12-02 15:02:25 -0500

    Created Expression class and header files

---
+++ Expression.cpp
@@ -0,0 +1,23 @@
+#include "Expression.h"
+
+Expression::Expression()
+{
+    //ctor
+}
+
+Expression::~Expression()
+{
+    //dtor
+}
+
+Expression::Expression(const Expression& other)
+{
+    //copy ctor
+}
+
+Expression& Expression::operator=(const Expression& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
