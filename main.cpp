0d58b1a8761ced152f77b813032259a3ecd727cd
VictorCarri <victorcarri@gmail.com>
2015-12-03 11:54:17 -0500

    Modified main.cpp to include some parsing code. I still need to plan out the parser.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+/* Forward declarations */
+bool validate(string toCheck); // Declare the prototype so that it can be used in main
+
 int main()
 {
     bool reading = true; // Controls input loop
@@ -18,10 +21,38 @@ int main()
             reading = false; // End the input loop
         }
 
-        else // Parse the input
+        /**
+        * The user's input was not a message to quit, so we need to first validate and then parse the expression.
+        **/
+        else
         {
-            // DEBUGGING
-            cout << "Your input was: \"" << input << "\"" << endl;
+            /* Only proceed if the input is valid */
+
+            if (validate(input)) // The expression is valid
+            {
+                cout << "Your expression is valid!" << endl; // Test code
+            }
+
+            else // The expression is invalid
+            {
+                cout << "Error: your expression is invalid!" << endl; // Test code
+            }
+        }
+    }
+}
+
+/** \brief Checks if a string is an expression.=
+ * \param toCheck - A string which contains the expression to validate.
+ * \return True if the string represents a valid expression, false if it doesn't.
+ */
+bool validate(string toCheck)
+{
+    int i = 0; // Current position in the string
+
+    while (i < toCheck.length()) // Loop through the whole string in a controlled manner
+    {
+        switch (toCheck[i]) // Perform different actions depending on the state of the machine and the current character
+        {
         }
     }
 }
