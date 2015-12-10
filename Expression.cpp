d9f32222bc186e178282a205e1d0c5d8063e8af8
Shilp Pancholi <shilp_10@hotmail.com>
2015-12-10 17:35:50 -0500

    Update Expression.cpp

--- Expression.cpp
+++ Expression.cpp
@@ -22,16 +22,11 @@ Expression& Expression::operator=(const Expression& rhs)
     return *this;
 }
 
-virtual string evaluate() { //evaluates the expression
+virtual string Expression:: evaluate() { //evaluates the expression
     return this->left.evaluate();//return answer as a string
 }
 
-virtual void print(); //prints the expression
-if { //check if expression is valid 
-    cout <<expression; << "= " << answer <<endl; //prints the original expression that equals the answer
-}
-//need a way to simplify the expression
-//also error checking.
-else {
-cout << "Expression is not valid" << endl; // if expression is not valid print this
+virtual void Expression:: print(); //prints the expression
+    cout <<left.print() <<"=" << right.print() <<endl; //prints the original expression that equals the answer
+    
 }
